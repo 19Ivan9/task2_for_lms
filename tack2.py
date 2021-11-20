b05addac7086fd0c7aa20663431baec7fbef8e12
ivan <iprokopenko711@gmail.com>
2021-11-20 01:49:17 +0100

    init

---
+++ tack2.py
@@ -0,0 +1,14 @@
+#Вывод "hallo world"
+print('hallo world')
+#Вывод "hallo world" с большой буквы т.е. "Нallo world"
+text = 'hallo world'
+text=text.capitalize()
+print(text)
+#Перенос строки после слова "are"
+print('how are \nyou?')
+#строка выведится с пробелами
+print('apple','banana','pineapple')
+
+#Вывод букв "О" и "Н" с помощью символов "#"
+print('#'*9+'\n'+"#\t\t#\n"*4+'#'*9)
+print(('#\t\t'*2+"\n")*2+"#"*9+'\n'+('#\t\t'*2+"\n")*2)
